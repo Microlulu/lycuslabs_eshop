9ab53a3a6ca7fee6d083db5c75bb2b876babbb76
Lucile <lucile.debuchy@gmail.com>
2022-03-09 18:49:34 +0100

    burger fixed profile to fix

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,5 @@
 
-// BOUTON PODUITS DYNAMIQUE
+/* BOUTON POUR PRODUITS DYNAMIQUE UTILISANT LA BASE DE DONNEE*/
     // Je crée un script pour mon bouton "products" : c'est un bouton dynamique qui prends en compte tous les produits existants et les affiches sous forme de dropdown.
     // À la demande de mon client, j'ai fait un bouton dynamique et pas de page affichant tous les produits comme les e-commerces traditionnels, car mon client est une start-up qui pour l'instant n'a qu'un seul produit.
     // Cette startup n'a pas pour projet de faire un site avec beaucoup de produits, le CEO m'a dit qu'au grand maximum, il y aurait 5 produits et tous seront très différents des uns des autres, d'où sa demande de les séparer dans des pages bien distincte sans liens entre eux.
@@ -22,13 +22,8 @@
 });
 
 
-// MENU BURGER TOGGLE
-/*function abc(){
-    var element = document.getElementById("myLinks");
-    element.classList.toggle("mystyle");
 
-}*/
-
+/* NAVBAR PRINCIPALE  : MENU BURGER TOGGLE */
 function burgerToggle() {
     let x = document.getElementById("myLinks");
     if (x.style.display == "block") {
@@ -39,37 +34,58 @@ function burgerToggle() {
 }
 
 
-/**/
-
-
-
-
-
-
-
-
-
-
-
-
-const ProductImg = document.getElementById("ProductImg");
-const SmallImg = document.getElementsByClassName("small-img");
-
-/*SmallImg[0].onclick =function ()
-{
-    ProductImg.src = SmallImg[0].src;
+/* NAVBAR DE L'UTILISATEUR DANS LA PAGE PROFILE : BOUTONS */
+function showSection(button){
+    //Je crée une fonction qui montrera les sections quand le boutons correspondant a la section sera cliqué.
+   let buttonID =  button.id;
+   //Pour chaque boutons :je prends l'id du boutons et je prends la section associée (l'élement) et je dis :
+    // display block sur la section que je veux afficher et display none sur toutes les autres.
+    // et je repète l'opération pour chaque bouton.
+    if(buttonID=="address-btn"){
+       document.getElementById("adresses").style.display="block";
+       document.getElementById("orders").style.display="none";
+       document.getElementById("licenses").style.display="none";
+       document.getElementById("downloads").style.display="none";
+       document.getElementById("support").style.display="none";
+   }
+   if(buttonID == "orders-btn"){
+       document.getElementById("adresses").style.display="none";
+       document.getElementById("orders").style.display="block";
+       document.getElementById("licenses").style.display="none";
+       document.getElementById("downloads").style.display="none";
+       document.getElementById("support").style.display="none";
+   }
+   if(buttonID == "downloads-btn"){
+       document.getElementById("adresses").style.display="none";
+       document.getElementById("orders").style.display="none";
+       document.getElementById("licenses").style.display="none";
+       document.getElementById("downloads").style.display="block";
+       document.getElementById("support").style.display="none";
+   }
+   if(buttonID == "licences-btn") {
+       document.getElementById("adresses").style.display = "none";
+       document.getElementById("orders").style.display = "none";
+       document.getElementById("licenses").style.display = "block";
+       document.getElementById("downloads").style.display = "none";
+       document.getElementById("support").style.display = "none";
+   }
+   if(buttonID == "support-btn"){
+       document.getElementById("adresses").style.display="none";
+       document.getElementById("orders").style.display="none";
+       document.getElementById("licenses").style.display="none";
+       document.getElementById("downloads").style.display="none";
+       document.getElementById("support").style.display="block";
+   }
 }
-SmallImg[1].onclick =function ()
-{
-    ProductImg.src = SmallImg[1].src;
-}
-SmallImg[2].onclick =function ()
-{
-    ProductImg.src = SmallImg[2].src;
-}
-SmallImg[3].onclick =function ()
-{
-    ProductImg.src = SmallImg[3].src;
+/* NAVBAR UTILISATEUR : MENU BURGER TOGGLE */
+/*function burgerToggleUser() {
+    let x = document.getElementById("myNavbar");
+    if (x.style.display == "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
 */
+
 
