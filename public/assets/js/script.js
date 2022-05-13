79c699751d38eb2d42110475cddecf5026571965
Lucile <lucile.debuchy@gmail.com>
2022-05-13 18:17:55 +0200

    mailjet configuration, probleme messageProfilController avec mailjet et ContactController pas de variable content, mettre en place je resetpassword mail

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,8 +1,10 @@
 
 /* BOUTON DROPDOWN DANS LA NAVBAR PRINCIPALE et POUR FOOTER POUR PRODUITS (BOUTON DYNAMIQUE UTILISANT LA BASE DE DONNEE)*/
 // Je crée un script pour mon bouton "products" : c'est un bouton dynamique qui prends en compte tous les produits existants et les affiches sous forme de dropdown.
-// À la demande de mon client, j'ai fait un bouton dynamique et pas de page affichant tous les produits comme les e-commerces traditionnels, car mon client est une start-up qui pour l'instant n'a qu'un seul produit.
-// Cette startup n'a pas pour projet de faire un site avec beaucoup de produits, le CEO m'a dit qu'au grand maximum, il y aurait 5 produits et tous seront très différents des uns des autres, d'où sa demande de les séparer dans des pages bien distincte sans liens entre eux.
+// À la demande de mon client, j'ai fait un bouton dynamique et pas de page affichant tous les produits comme les e-commerces traditionnels, car mon client est une
+// start-up qui pour l'instant n'a qu'un seul produit.
+// Cette startup n'a pas pour projet de faire un site avec beaucoup de produits, le CEO m'a dit qu'au grand maximum, il y aurait 5 à 7 produits et tous seront très
+// différents des uns des autres, d'où sa demande de les séparer dans des pages bien distincte sans liens entre eux.
 fetch('/api/products')
     .then(response => {
         return response.json()
@@ -13,7 +15,8 @@ fetch('/api/products')
         // je crée un text vide et je dis dans ma boucle += qui veut dire ajoute, mais ne supprime pas l'ancien produit
         let text = ""
         for (index in products) {
-            // et la je viens créer mon ma balise li dans lequel je mets un lien a et je viens faire ma boucle sur mes produits, je dis prends l'id d'un produit dans l'index Products et ajoute son titre et un + pour dire répète cette action dans un
+            // et la je viens créer mon ma balise li dans lequel je mets un lien a et je viens faire ma boucle sur mes produits, je dis prends l'id d'un produit dans
+            // l'index Products et ajoute son titre et un + pour dire répète cette action dans un
             // autre li pour chaque produits
             text += "<li>" +
                 "<a href='/ourproduct/" + products[index].slug + "'>" + products[index].title + "</a>" +
